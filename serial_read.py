668a900acafdd7128fc9f369f0bce3a9b328672f
Ronald Karam <ronald.karam1212@gmail.com>
2022-03-14 20:52:18 +0200

    added Serial reading

---
+++ serial_read.py
@@ -0,0 +1,16 @@
+import serial
+import serial.tools.list_ports
+
+ser = serial.Serial('COM3', baudrate=9600, bytesize=8)  # open WINDOWS serial port
+#ser = serial.Serial('/dev/ttyACM0', baudrate=9600, bytesize=8)  # open LINUX serial port
+
+print(ser.name)         # check which port was really used
+i = 0
+while True:
+    x = ser.readline()
+    print("pot: ", x.decode('utf-8').rstrip())
+    if i == 1000:
+        ser.close()
+    i+=1
+
+#ser.close()             # close port
