569a01663299a028a91fd63462a854a61219df36
Ronald Karam <ronald.karam1212@gmail.com>
2022-03-21 18:02:52 +0200

    Making login process card-only and no popups

--- pages/data_retrieval_page.py
+++ pages/data_retrieval_page.py
@@ -5,10 +5,7 @@ import sqlite3
 from datetime import datetime
 import pathlib
 
-
-import RPi.GPIO as GPIO
-from mfrc522 import SimpleMFRC522
-reader = SimpleMFRC522()
+#from ..scan_card import scan_card
 
 
 path = pathlib.Path(__file__).parent.absolute()
@@ -99,21 +96,6 @@ def draw_data(x_pos,y_pos):
     dor_data.config(text = dor, background="#292929", foreground = "#ffc815")
     print("Data displayed")
 
-def scan_card():
-    print("Card scanner initiated")
-    global using_card, login_data, card_id
-    if messagebox.askokcancel(title="Card Scanner", message= "Press OK to start scanning for card."):
-        #WRITE CARD SCAN CODE 
-        user_id_label.config(text = "")
-     #   card_id = input("Enter used ID to simulate card swipe: ")
-        card_id = reader.read()[0]
-        user_id_label.config(text = card_id)
-    else:
-        quit()
-    print("Card Scanned:", card_id)
-
-    
-
 def draw_login_form(x_pos, y_pos):
     global login_info_field
     if not login_info_field  == None:
@@ -121,30 +103,6 @@ def draw_login_form(x_pos, y_pos):
     login_info_field = ttk.Entry()
     login_info_field.place(x = x_pos,y = y_pos ,width=300, anchor = "center")
     print("Login form generated")
-
-def choose_login_method():
-    login_method_menu = Menubutton(root, text = "Choose a login method")
-    login_method_menu.menu = Menu(login_method_menu)
-    login_method_menu["menu"] = login_method_menu.menu
-    login_method_menu.menu.add_radiobutton(label = "Email", variable = login_method, value = 0, command=get_user)
-    login_method_menu.menu.add_radiobutton(label = "Phone", variable = login_method, value = 1, command=get_user)
-    login_method_menu.menu.add_radiobutton(label = "Card", variable = login_method, value = 2, command=get_user)
-    login_method_menu.place(x = x_size/2, y = (y_size/2)+30, anchor="center")
-
-def get_user():
-    global login_info_field
-    if (login_method.get() == 2):
-        if not login_info_field  == None:
-            login_info_field.destroy()
-            login_info_field = None
-        scan_card()
-        draw_buttons(x_size/2,(y_size/2)+100)
-    elif (login_method.get() == 1):
-        draw_login_form(x_size/2,(y_size/2)+50)
-        draw_buttons(x_size/2,(y_size/2)+100)
-    else:
-        draw_login_form(x_size/2,(y_size/2)+50)
-        draw_buttons(x_size/2,(y_size/2)+100)
 
 def get_user_info():
     x_pos = (x_size/5)+30
@@ -174,7 +132,6 @@ def draw_buttons(x_pos, y_pos):
 
 def main():
     draw_decorations()
-    choose_login_method()
     draw_labels((x_size/5)+30,y_size/5)
     
     root.mainloop()
