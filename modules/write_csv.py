e745c1468e6cd90287eebbc3441e0200cea2825d
Ronald Karam <ronald.karam1212@gmail.com>
2022-03-30 18:14:59 +0300

    working on write csv

--- modules/write_csv.py
+++ modules/write_csv.py
@@ -1,6 +1,4 @@
 import csv
-from tkinter import E
-from venv import create
 import serial
 import pathlib
 import datetime
@@ -8,60 +6,51 @@ import datetime
 #===================================================================#
 #                            FUNCTIONS                              #
 #===================================================================#
-#Create unique numbered CSV file every time the program is run
 
-def create_filename(user_id, date):
+
+def create_filename(user_id, date): #Create unique numbered CSV file every time the program is run
     filename = str(user_id + "_" + date + "_sensor_data.csv")
     return filename
 
-def save_data():
+def save_data(filename, header):   #writes data to csv file
+    filepath = path / "Sensor Logs" / filename  
+    data_file = open(filepath, 'w', newline='') #creates a new file in write mode 
     writer = csv.writer(data_file)
     writer.writerow(header)
     while True:
         writer.writerow(read_serial())
 
-
-def read_serial(): 
+def read_serial():  #reads serial input 
     data_string = ser.readline().decode('utf-8').rstrip()
     data_list = data_string.split(",")
     return data_list
 
-def handler(signum, frame):
-    data_file.close()
+def handler(signum, frame): #if ctrl+c was used to end the program, need to use another way
+    #data_file.close()   #IMPORTANT !!! Required to use in the end
     print("DATA FILE SAVED AND CLOSED!")
-
 
 #===================================================================#
 #                            VARIABLES                              #
 #===================================================================#
 
 # SERIAL STUFF
-ser = serial.Serial('COM6', baudrate=9600, bytesize=8)  # open WINDOWS serial port
-#ser = serial.Serial('/dev/ttyACM0', baudrate=9600, bytesize=8)  # open LINUX serial port
-#x = ser.readline()
-#x.decode('utf-8').rstrip()
-#ser.close()
-
-
+#ser = serial.Serial('COM6', baudrate=9600, bytesize=8)  # open WINDOWS serial port
+ser = serial.Serial('/dev/ttyACM0', baudrate=9600, bytesize=8)  # open LINUX serial port
 
 date = datetime.datetime.now()
 datestring = date.strftime("%y%m%d%H%M%S")
-user_id = "test"
+#user_id = "test"
+
 # CSV STUFF
-filename = create_filename(user_id, datestring)
+#filename = create_filename(user_id, datestring)
 
 #Filepath Stuff
-path = pathlib.Path(__file__).parent.absolute()
-filepath = path / "Sensor Logs" / filename
+path = pathlib.Path(__file__).parent.absolute() #gets current path
 
-data_file = open(filepath, 'w', newline='')
-writer = csv.writer(data_file)
+
+
+
 
 header = ["Time (ms)","Encoder Counter", "RPM", "Voltage (mV)"]
 
-
-
-
-
-
-save_data()
+#save_data()
