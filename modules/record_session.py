6a1b65035c09bed27f4296b134769944529a8552
RonaldK1212 <ronald.karam1212@gmail.com>
2022-03-30 23:24:53 +0300

    Scan card and record session complete

--- modules/record_session.py
+++ modules/record_session.py
@@ -1,8 +1,20 @@
-from write_csv import create_filename, save_data, read_serial, header
+from write_csv import create_filename, save_data, read_serial, header, path
 from scan_card import scan_card
 import datetime
+import sqlite3
+import pathlib
+
+db_path = path / '..' / 'meta_gym.db'
+
+con = sqlite3.connect(db_path)
+cur = con.cursor()
 
 date = datetime.datetime.now()  #gets current date
 datestring = date.strftime("%y%m%d%H%M%S")  #formats date
+user_id = scan_card()
+filename = create_filename(user_id, datestring)
 
-save_data(create_filename(scan_card(), datestring), header)
+cur.execute("INSERT INTO sessions VALUES (?,?,?)", (filename, user_id, date))
+con.commit()
+con.close()
+save_data(filename, header)
--- modules/write_csv.py
+++ modules/write_csv.py
@@ -13,34 +13,32 @@ def create_filename(user_id, date): #Create unique numbered CSV file every time 
     return filename
 
 def save_data(filename, header):   #writes data to csv file
-    filepath = path / ".." / "Sensor Logs" / filename  
+    filepath = path / '..' / "Sensor Logs" / filename  
     data_file = open(filepath, 'w', newline='') #creates a new file in write mode 
     writer = csv.writer(data_file)
     writer.writerow(header)
     ser.flush()
-    while True:
-        writer.writerow(read_serial())
+    try:
+        while True:
+            writer.writerow(read_serial())
+    except KeyboardInterrupt:
+        data_file.close()
+        print("File saved and closed!")
+        exit()
 
 def read_serial():  #reads serial input 
     data_string = ser.readline().decode('utf-8').rstrip()
     data_list = data_string.split(",")
     return data_list
 
-def handler(signum, frame): #if ctrl+c was used to end the program, need to use another way
-    save_close()
-    
-
-def save_close(data_file):
-    data_file.close()
-    print("DATA FILE SAVED AND CLOSED!")
 #===================================================================#
 #                            VARIABLES                              #
 #===================================================================#
 
 
 # SERIAL STUFF
-ser = serial.Serial('COM3', baudrate=9600, bytesize=8)  # open WINDOWS serial port
-#ser = serial.Serial('/dev/ttyACM0', baudrate=9600, bytesize=8)  # open LINUX serial port
+#ser = serial.Serial('COM3', baudrate=9600, bytesize=8)  # open WINDOWS serial port
+ser = serial.Serial('/dev/ttyACM0', baudrate=9600, bytesize=8)  # open LINUX serial port
 date = datetime.datetime.now()  #gets current date
 datestring = date.strftime("%y%m%d%H%M%S")  #formats date
 path = pathlib.Path(__file__).parent.absolute() #gets current path
