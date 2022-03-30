54d38a10949b53da299f42e264db14319751253a
Ronald Karam <ronald.karam1212@gmail.com>
2022-03-30 20:13:32 +0300

    Save session using card id

--- modules/record_session.py
+++ modules/record_session.py
@@ -1,3 +1,8 @@
-import write_csv
-import scan_card
+from write_csv import create_filename, save_data, read_serial, header
+from scan_card import scan_card
+import datetime
 
+date = datetime.datetime.now()  #gets current date
+datestring = date.strftime("%y%m%d%H%M%S")  #formats date
+
+save_data(create_filename(scan_card(), datestring), header)
--- modules/write_csv.py
+++ modules/write_csv.py
@@ -9,14 +9,15 @@ import datetime
 
 
 def create_filename(user_id, date): #Create unique numbered CSV file every time the program is run
-    filename = str(user_id + "_" + date + "_sensor_data.csv")
+    filename = str(str(user_id) + "_" + date + "_sensor_data.csv")
     return filename
 
 def save_data(filename, header):   #writes data to csv file
-    filepath = path / "Sensor Logs" / filename  
+    filepath = path / ".." / "Sensor Logs" / filename  
     data_file = open(filepath, 'w', newline='') #creates a new file in write mode 
     writer = csv.writer(data_file)
     writer.writerow(header)
+    ser.flush()
     while True:
         writer.writerow(read_serial())
 
@@ -26,31 +27,23 @@ def read_serial():  #reads serial input
     return data_list
 
 def handler(signum, frame): #if ctrl+c was used to end the program, need to use another way
-    #data_file.close()   #IMPORTANT !!! Required to use in the end
-    print("DATA FILE SAVED AND CLOSED!")
+    save_close()
+    
 
+def save_close(data_file):
+    data_file.close()
+    print("DATA FILE SAVED AND CLOSED!")
 #===================================================================#
 #                            VARIABLES                              #
 #===================================================================#
 
+
 # SERIAL STUFF
-#ser = serial.Serial('COM6', baudrate=9600, bytesize=8)  # open WINDOWS serial port
-ser = serial.Serial('/dev/ttyACM0', baudrate=9600, bytesize=8)  # open LINUX serial port
-
-date = datetime.datetime.now()
-datestring = date.strftime("%y%m%d%H%M%S")
-#user_id = "test"
-
-# CSV STUFF
-#filename = create_filename(user_id, datestring)
-
-#Filepath Stuff
+ser = serial.Serial('COM3', baudrate=9600, bytesize=8)  # open WINDOWS serial port
+#ser = serial.Serial('/dev/ttyACM0', baudrate=9600, bytesize=8)  # open LINUX serial port
+date = datetime.datetime.now()  #gets current date
+datestring = date.strftime("%y%m%d%H%M%S")  #formats date
 path = pathlib.Path(__file__).parent.absolute() #gets current path
+header = ["Time (ms)","Encoder Counter", "RPM", "Voltage (mV)"] #csv columns/header
 
-
-
-
-
-header = ["Time (ms)","Encoder Counter", "RPM", "Voltage (mV)"]
-
-#save_data()
+#save_data(create_filename("7162591", datestring), header)
